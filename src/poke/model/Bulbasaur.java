857026452cf29d3d1fcc2dfb5e71c514a049cd3e
darynamm <darynamanuilenko@gmail.com>
2023-01-24 08:39:15 -0700

    did eevee and bulb file

---
+++ src/poke/model/Bulbasaur.java
@@ -0,0 +1,23 @@
+package poke.model;
+
+public class Bulbasaur extends Pokemon
+{
+
+	public Bulbasaur()
+	{
+		super(194, "Bulbasaur");
+	}
+	public Eevee(String name)
+	{
+		super(133, name);
+		
+	}
+
+	public Eevee(int number, String name)
+	{
+		super(number, name);
+	}
+
+	}
+
+
