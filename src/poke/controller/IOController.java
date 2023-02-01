e5fa0afeab5a1865c4c03d62b71c68237acde938
darynamm <darynamanuilenko@gmail.com>
2023-02-01 09:28:51 -0700

    downcasting objects to an array list of pokemon

--- src/poke/controller/IOController.java
+++ src/poke/controller/IOController.java
@@ -17,11 +17,39 @@ public class IOController
 	public static ArrayList<Pokemon> loadData(String dataFile, PokeFrame frame)
 	{
 		ArrayList<Pokemon> pokeList = null;
+		
+		try (FileInputStream fileStream = new FileInputStream(dataFile);
+				ObjectInputStream objctStream = new ObjectInputStream(fileStream)
+		{
+			ArrayList<Pokemon> loadedPokemon = new ArrayList,Pokemon>();
+			loadedPokemon = (ArrayList<Pokemon>) objectStream.readObject();
+			pokeList = loadedPokemon;
+		}
+		catch(IOException fileReadEror)
+		
+		{
+			JOptionPane.showMessageDialog(frame,  fileReadError.getMessage(), "Could not read the file!!", JOptionPane.ERROR_MESSAGE);
+		}
+		catch (ClassNotFoundException classError)
+		{
+			JOptionPane.showMessageDialog(frame,  classError.getMessage(), "OMG class error!", JOptionPane.ERROR_MESSAGE);
+		}
 		return pokeList;
 	}
 	
 	public static void saveData(String dataFile, ArrayList<Pokemon> pokemonList, PokeFrame frame)
 	{
+		
+		try (FileOutputStream fileStream = new FileOutputStream(dataFile);
+				ObjectOutputStream objectStream = new ObjectOutputStream(fileStream))
+		{
+			objectStream.writeObject(pokemonList);
+		}
+		catch (IOException fileError)
+		{
+			JOptionPane.showMessageDialog(frame, "Error saving file!");
+		}
+		
 		
 	}
 }
